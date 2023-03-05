11e7c4a179905bd4ec716877d49600b3f6207d88
Lolo <lowgau@gmail.com>
2023-03-05 13:27:50 +0100

    improved shell script

--- run.sh
+++ run.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-nohup "python3.9 bot.py" &
+executed=0
+while [ "$executed" == 0 ] do :
+    if [ $(date +%M) -eq 00 ]; then
+        echo "Executing."
+        executed=1
+        nohup "python3.9 bot.py" &
+    fi
+    sleep 60
+done
